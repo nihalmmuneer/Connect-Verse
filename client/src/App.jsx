dadfba253e62dc3c711a0c7acca1af11b79f6e33
nihal <nihalmmuneer@gmail.com>
2024-07-07 15:12:30 +0530

    completion of frontend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,42 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Project from "./pages/Project";
+import SignIn from "./pages/SignIn";
+import Dashboard from "./pages/Dashboard";
+import SignUp from "./pages/SignUp";
+import Header from "./components/Header";
+import FooterComponent from "./components/FooterComponent";
+import PrivateRouter from "./components/PrivateRouter";
+import IsAdminPrivateRouteOnly from "./components/isAdminPrivateRouteOnly";
+import CreatePost from "./pages/CreatePost";
+import UpdatePost from "./pages/UpdatePost";
+import PostPage from "./pages/PostPage";
+import ScrollToTop from "./components/ScrollToTop";
+import Search from "./pages/Search";
 
-const Home = () => {
+export default function App() {
   return (
-    <div className="text-red-500">Home</div>
-  )
+    <BrowserRouter>
+      <ScrollToTop />
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/about" element={<About />}></Route>
+        <Route path="/projects" element={<Project />}></Route>
+        <Route path="/sign-in" element={<SignIn />}></Route>
+        <Route path="/search" element={<Search />}></Route>
+        <Route element={<PrivateRouter />}>
+          <Route path="/dashboard" element={<Dashboard />}></Route>
+        </Route>
+        <Route element={<IsAdminPrivateRouteOnly />}>
+          <Route path="/create-post" element={<CreatePost />} />
+          <Route path="/update-posts/:postId" element={<UpdatePost />} />
+        </Route>
+        <Route path="/sign-up" element={<SignUp />}></Route>
+        <Route path="/posts/:postSlug" element={<PostPage />}></Route>
+      </Routes>
+      <FooterComponent />
+    </BrowserRouter>
+  );
 }
-
-export default Home
